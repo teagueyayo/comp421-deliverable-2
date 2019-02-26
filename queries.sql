72cc411a5519e9e36d67e2e9559d5515c3fab17a
Terrence Gao <qi.gao@mail.mcgill.ca>
2019-02-25 23:24:37 -0500

    Update queries.sql

--- queries.sql
+++ queries.sql
@@ -5,3 +5,20 @@ WHERE sin IN
 	 FROM buys B, house H
 	 WHERE  (B.price >= 1000000) AND 
 	        (H.address = B.laddress))
+
+SELECT aame, SUM(price)
+FROM buys 
+GROUP BY aame
+
+
+SELECT F.name, B.SUM(price)
+FROM real_estate_firm F, buys B, agent A
+WHERE (A.company_id == F.id)
+GROUP BY F.id
+
+SELECT A.address 
+FROM apt_building A, listing L
+WHERE (A.address == L.address) 
+ORDER BY L.price DESC	
+LIMIT 5
+
