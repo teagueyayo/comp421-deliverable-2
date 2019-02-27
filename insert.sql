864a4581bce33c7dd82921a09696927658de75ae
Will-Guthrie <william.guthrie@mail.mcgill.ca>
2019-02-27 17:53:28 -0500

    Add insert commands

--- insert.sql
+++ insert.sql
@@ -1,6 +1,35 @@
-INSERT INTO real_estate_firm 
-	(id, name, address, brokerage_number)
-VALUES ("1", "BPAL", "200 Avenue Laurier O #375", "102");
+--Insert generic companies--
+INSERT INTO company
+	(id, name, address)
+VALUES (1, 'Amazon', '101 Amazon Rd.' );
+
+INSERT INTO company
+	(id, name, address)
+VALUES (2, 'Google', '1 Google St.' );
+
+INSERT INTO company
+	(id, name, address)
+VALUES (3, 'Not_relevant_company', '322 McGill Lane' );
+
+INSERT INTO company
+	(id, name, address)
+VALUES (4, 'Apple', '10 Super Place' );
+
+INSERT INTO company
+	(id, name, address)
+VALUES (5, 'Facebook', '666 Normal St.' );
+
+--Insert real estate companies--
+
+INSERT INTO company
+	(id, name, address)
+VALUES (6, 'Boys Real Estate', '101 Nunya Buisness Rd.');
+INSERT INTO real_estate_firm
+	(brokerage_number, id)
+VALUES ('8838', 6);
+
+
+--ETC--
 
 INSERT INTO manager (name)
 VALUES ("Terrence Gao");
