591b0a88ad115b15e87debda1d5689ff786d43e7
Will-Guthrie <william.guthrie@mail.mcgill.ca>
2019-02-28 14:17:28 -0500

    Remove inserts that are duplicated in insert.sql

--- data_population.sql
+++ data_population.sql
@@ -103,23 +103,7 @@ VALUES (15);
 
 INSERT INTO manager
 	(name)
-VALUES ('David The King Manager');
-
-INSERT INTO manager
-	(name)
-VALUES ('Will Manager');
-
-INSERT INTO manager
-	(name)
-VALUES ('Aidon Manager');
-
-INSERT INTO manager
-	(name)
-VALUES ('Seasy Manager');
-
-INSERT INTO manager
-	(name)
-VALUES ('Terry Manager');
+VALUES ('New Manager');
 
 
 --Insert appartment buildings--
