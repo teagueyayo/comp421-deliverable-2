eccbbb4fa792861b986311ff43a70eff957f2f98
Aidon Lebar <aidonlebar@gmail.com>
2019-02-28 12:55:51 -0500

    Test and fix up queries. Add comment explaining what they do.

--- queries.sql
+++ queries.sql
@@ -1,24 +1,26 @@
-SELECT name 
-FROM customer 
-WHERE sin IN  
-	(SELECT B.csin 
+--return the name of any customer has bought a house for > $1M
+SELECT name
+FROM customer
+WHERE sin IN
+	(SELECT B.csin
 	 FROM buys B, house H
-	 WHERE  (B.price >= 1000000) AND 
+	 WHERE  (B.price >= 1000000) AND
 	        (H.address = B.laddress))
 
-SELECT aame, SUM(price)
-FROM buys 
-GROUP BY aame
+--return total sales amount per agent sorted in descending order
+SELECT aname, SUM(price)
+FROM buys
+GROUP BY aname ORDER BY SUM(price) DESC;
 
+--return total sales from every real estate in descending order
+SELECT C.name, SUM(B.price) --TODO
+FROM real_estate_firm F, buys B, agent A, company C
+WHERE (A.company_id = F.id) AND (C.id = A.company_id)
+GROUP BY C.id ORDER BY sum(B.price) DESC;
 
-SELECT F.name, B.SUM(price)
-FROM real_estate_firm F, buys B, agent A
-WHERE (A.company_id == F.id)
-GROUP BY F.id
-
-SELECT A.address 
+--address address of the apt buildings with the top 3 most expensive units
+SELECT A.address --TODO fix
 FROM apt_building A, listing L
-WHERE (A.address == L.address) 
-ORDER BY L.price DESC	
-LIMIT 5
-
+WHERE (A.address = L.address)
+ORDER BY L.list_price DESC
+LIMIT 3;
