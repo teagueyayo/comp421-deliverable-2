6f81913936bf3ec71702815cf9830441e1022986
Will-Guthrie <william.guthrie@mail.mcgill.ca>
2019-03-01 14:53:01 -0500

    Add checks log files

---
+++ testing_checks.sql
@@ -0,0 +1,23 @@
+INSERT INTO buys
+	(price, aname, csin, laddress)
+VALUES (-200, 'Agent Dave', 2919203, '404 House Rd.');
+
+INSERT INTO sells
+	(price, aname, csin, laddress)
+VALUES (0, 'Agent Simon', 2919204, '101 House Rd.');
+
+INSERT INTO listing
+	(address, build_date, size, list_price)
+VALUES ('523 Apprtment Building St. App. 32', '2002-07-09', -1, 230000);
+
+INSERT INTO listing
+	(address, build_date, size, list_price)
+VALUES ('523 Apprtment Building St. App. 32', '2002-07-09', 3000, -12300);
+
+INSERT INTO house
+	(stories, lot_size, address)
+VALUES (0, 2500, '202 House Rd.');
+
+INSERT INTO house
+	(stories, lot_size, address)
+VALUES (2, -230, '202 House Rd.');
